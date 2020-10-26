844f83823f449b06daa145c19409e48ae516e005
Gilberto Bá <impactodesigner2020@gmail.com>
2020-10-26 15:59:50 -0300

    primeiro commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,39 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './components/views/home/home.component';
+import { ContatosComponent } from './components/views/contatos/contatos.component';
+import { ServicosComponent } from './components/views/servicos/servicos.component';
+import { FotosComponent } from './components/views/fotos/fotos.component';
+import { LocalComponent } from './components/views/local/local.component';
+
+const routes: Routes = [
+  
+  {
+    path:"",
+    component: HomeComponent
+  },
+  {
+    path:"home",
+    component: HomeComponent
+  },
+  {
+    path:"contatos",
+    component: ContatosComponent
+  },
+  {
+    path:"servicos",
+    component: ServicosComponent
+  },
+  {
+    path:"fotos",
+    component: FotosComponent
+  },
+  {
+    path:"local",
+    component: LocalComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/template/header/header.component';
+import { FooterComponent } from './components/template/footer/footer.component';
+import { NavComponent } from './components/template/nav/nav.component';
+import { HomeComponent } from './components/views/home/home.component';
+import { ContatosComponent } from './components/views/contatos/contatos.component';
+import { ServicosComponent } from './components/views/servicos/servicos.component';
+import { FotosComponent } from './components/views/fotos/fotos.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LocalComponent } from './components/views/local/local.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    HomeComponent,
+    ContatosComponent,
+    ServicosComponent,
+    FotosComponent,
+    LocalComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
